cdfd5b962576c7bf46e3f15db249f7160e8a75cc
Bob Underhill <roberthill@ou.edu>
2018-11-14 14:19:36 -0600

    i did it

---
+++ MyFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame
+{
+
+    private MyPanel panel;
+    private final int WIDTH = 300;
+    private final int LENGTH = 400;
+    
+    public MyFrame(String title)
+    {
+        setTitle(title);
+        setSize(WIDTH, LENGTH);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        
+        MyFrame frame = new MyFrame("Hello");
+
+    }
+
+}
